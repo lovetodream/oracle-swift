b4ebe6db6332d32948d5b0e6676d23e7946c250e
Timo <38291523+lovetodream@users.noreply.github.com>
2022-12-10 21:02:55 +0100

    chore(deps): add odpi-c

--- Package.swift
+++ Package.swift
@@ -13,11 +13,13 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
     ],
     targets: [
+        .target(name: "ODPIC"),
         .target(
             name: "OracleNIO",
             dependencies: [
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "NIO", package: "swift-nio"),
+                .target(name: "ODPIC"),
             ]),
         .testTarget(
             name: "OracleNIOTests",
